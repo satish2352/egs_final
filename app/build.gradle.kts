8f4befd916199d91b22803217cfda2db2f9938d6
Iamthemax <vugore@gmail.com>
2024-03-08 10:50:38 +0530

    After working qr scan

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,9 +50,14 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.firebase:firebase-firestore-ktx:24.10.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.google.android.gms:play-services-mlkit-barcode-scanning:18.3.0")
+    implementation("com.google.mlkit:barcode-scanning:17.2.0")
+    implementation ("androidx.camera:camera-camera2:1.2.1")
+    implementation ("androidx.camera:camera-lifecycle:1.2.1")
+    implementation ("androidx.camera:camera-view:1.2.1")
 
 }
--- app/src/main/java/com/sumagoinfotech/digicopy/ui/activities/ScanBarcodeActivity.kt
+++ app/src/main/java/com/sumagoinfotech/digicopy/ui/activities/ScanBarcodeActivity.kt
@@ -3,11 +3,14 @@ package com.sumagoinfotech.digicopy.ui.activities
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -16,131 +19,73 @@ import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
 import com.sumagoinfotech.digicopy.R
+import com.sumagoinfotech.digicopy.databinding.ActivityScanBarcodeBinding
 import java.util.concurrent.Executors
 
 class ScanBarcodeActivity : AppCompatActivity() {
-    private val CAMERA_PERMISSION_REQUEST_CODE = 200
-    private lateinit var cameraPreview: SurfaceView
-    private lateinit var barcodeTextView: TextView
-    private lateinit var barcodeScanner: BarcodeScanner
-    private val executor = Executors.newSingleThreadExecutor()
+    private lateinit var binding:ActivityScanBarcodeBinding
+    private val cameraPermission = android.Manifest.permission.CAMERA
 
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+        if (isGranted) {
+            startScanner()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_scan_barcode)
+        binding = ActivityScanBarcodeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        cameraPreview = findViewById(R.id.cameraPreview)
-        barcodeTextView = findViewById(R.id.barcodeTextView)
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-            != PackageManager.PERMISSION_GRANTED
-        ) {
-            // Permission is not granted
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.CAMERA),
-                CAMERA_PERMISSION_REQUEST_CODE
-            )
-        } else {
-            // Permission has already been granted
-            initializeCamera()
+        binding.buttonOpenScanner.setOnClickListener {
+            requestCameraAndStartScanner()
+            startScanner()
         }
     }
 
-    private fun initializeCamera() {
-        val options = BarcodeScannerOptions.Builder()
-            .setBarcodeFormats(Barcode.FORMAT_ALL_FORMATS)
-            .build()
-
-        barcodeScanner = com.google.mlkit.vision.barcode.BarcodeScanning.getClient(options)
-
-        cameraPreview.holder.addCallback(object : SurfaceHolder.Callback {
-            override fun surfaceCreated(holder: SurfaceHolder) {
-                startCamera()
-            }
-
-            override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {}
-
-            override fun surfaceDestroyed(holder: SurfaceHolder) {
-                // release resources
-            }
-        })
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun requestCameraAndStartScanner() {
+//        if (isPermissionGranted(cameraPermission)) {
+//            startScanner()
+//        } else {
+//            requestCameraPermission()
+//        }
     }
 
-    private fun startCamera() {
-        try {
-            if (ActivityCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.CAMERA
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                return
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun requestCameraPermission() {
+        when {
+            shouldShowRequestPermissionRationale(cameraPermission) -> {
+//                cameraPermissionRequest(
+//                    positive = { openPermissionSetting() }
+//                )
             }
-            cameraPreview.holder.setKeepScreenOn(true)
-            cameraPreview.holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS)
-            cameraPreview.holder.addCallback(object : SurfaceHolder.Callback {
-                override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {}
-
-                override fun surfaceDestroyed(holder: SurfaceHolder) {}
-
-                override fun surfaceCreated(holder: SurfaceHolder) {
-                    cameraScanner(holder)
-                }
-            })
-        } catch (e: Exception) {
-            Log.d("Exception", "startCamera: " + e.message)
+            else -> {
+                requestPermissionLauncher.launch(cameraPermission)
+            }
         }
     }
 
-    private fun cameraScanner(holder: SurfaceHolder) {
-        val inputImage = InputImage.fromBitmap(cameraPreview.drawingCache, 0)
-        barcodeScanner.process(inputImage)
-            .addOnSuccessListener { barcodes ->
-                for (barcode in barcodes) {
-                    val bounds = barcode.boundingBox
-                    val corners = barcode.cornerPoints
-
-                    val rawValue = barcode.rawValue
-                    val valueType = barcode.valueType
-
-                    // Extract barcode data
-                    val displayValue = barcode.displayValue
-                    barcodeTextView.text = displayValue
-                    barcodeTextView.visibility = TextView.VISIBLE
-                }
-            }
-            .addOnFailureListener {
-                // Task failed with an exception
-                // ...
-            }
-            .addOnCompleteListener {
-                cameraPreview.holder.addCallback(object : SurfaceHolder.Callback {
-                    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {}
-
-                    override fun surfaceDestroyed(holder: SurfaceHolder) {}
-
-                    override fun surfaceCreated(holder: SurfaceHolder) {
-                        cameraScanner(holder)
+    private fun startScanner() {
+        ScannerActivity.startScanner(this) { barcodes ->
+            barcodes.forEach { barcode ->
+                when (barcode.valueType) {
+                    Barcode.TYPE_URL -> {
+                        binding.textViewQrType.text = "URL"
+                        binding.textViewQrContent.text = barcode.url.toString()
                     }
-                })
-            }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission granted, initialize the camera
-                initializeCamera()
-            } else {
-                // Permission denied
-                // You might want to show a message to the user indicating that permission is required
+                    Barcode.TYPE_CONTACT_INFO -> {
+                        binding.textViewQrType.text = "Contact"
+                        binding.textViewQrContent.text = barcode.contactInfo.toString()
+                    }
+                    else -> {
+                        binding.textViewQrType.text = "Other"
+                        binding.textViewQrContent.text = barcode.rawValue.toString()
+                    }
+                }
             }
         }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        executor.shutdown()
     }
 }
+
